6ac23f580c08aacec9b6c68fc59618ddb53fca21
juhyun <realeap91@gmail.com>
2021-04-15 05:15:39 +0900

    handler and port

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("text");
-console.log("result")
+const express = require('express');
+//load exprss folder
+const app = express();
+// excutive result of object save in app
+const PORT = 4000;
+// open port 4000
+function handleListening() {
+    console.log('Listening on: http://localhost:${PORT}');
+}
+// notice server is running
+app.listen(PORT, handleListening());
+//excutive port and handlerlistening
